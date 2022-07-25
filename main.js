750957be53dae84cfb5d0de36a61ed13bc5f6371
shacharbi23-meet <109904744+shacharbi23-meet@users.noreply.github.com>
2022-07-25 18:53:22 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-function changeBackgroundColor(color) {
-	document.body.style.background = color;
+function changeBackgroundColor() {
+	
+	if (document.body.style.backgroundColor == "blue") {
+		document.body.style.backgroundColor="red";
+		document.getElementById("sekaiSwitch").innerHTML = "Switch to the School Sekai";
+	}
+	else if (document.body.style.backgroundColor != "blue"){
+		document.body.style.backgroundColor = "blue";
+		document.getElementById("sekaiSwitch").innerHTML = "Switch to the Street Sekai";
+	}
+
+	//document.getElementById("sekaiSwitch").innerHTML = "Switch to the Street Sekai";
+		
 }
